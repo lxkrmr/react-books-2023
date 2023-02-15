c80b5977884b99632079b0f4fa06d152866a188a
Alex <lxkrmr@icloud.com>
2023-02-15 15:07:01 +0100

    ✨ use effect to initialize books on startup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
   const createBook = async (title) => {
     const response = await axios.post('http://localhost:3001/books', {
